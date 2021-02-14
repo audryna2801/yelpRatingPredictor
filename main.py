b03906c4e8763eb3577244e5f18d8fe3657ba45a
Julianna Eng <juliannaeng@Juliannas-MacBook-Pro.local>
2021-02-14 11:18:19 -0600

    oops

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 # this is where we will build our project :)
-<<<<<<< HEAD
-print("yelp")
-=======
 
 print('Hello world!')
 
 # leaving a test comment
->>>>>>> de6344b25eaefa5740192b4de0512b384ffbf428

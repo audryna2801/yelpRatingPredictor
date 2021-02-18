05e8aa5d0d38b6836bb0a732893ab9a69eb47e06
Rhedintza Audryna <rhedintzaaudryna@Rhedintzas-MacBook-Pro.local>
2021-02-18 10:23:31 +0700

    Finished analyze_words

--- analyze_words.py
+++ analyze_words.py
@@ -12,10 +12,6 @@ import nltk
 nltk.download('wordnet')
 
 
-STOP_WORDS = ["a", "an", "the", "this", "that", "of", "for", "or",
-              "and", "on", "to", "be", "if", "we", "you", "in", "is",
-              "at", "it", "rt", "mt", "with"]
-
 STOP_PREFIXES = ("@", "#", "http", "&amp")
 
 
@@ -29,9 +25,6 @@ def keep_chr(ch):
 
 PUNCTUATION = " ".join([chr(i) for i in range(sys.maxunicode)
                         if keep_chr(chr(i))])
-
-data = pd.read_csv("./test_data/babareba.csv")
-df = pd.DataFrame(data, columns=['Rating', 'Text'])
 
 
 def processing(text):
@@ -48,8 +41,8 @@ def processing(text):
     lemmatizer = WordNetLemmatizer()
 
     # Try to fix spelling errors before splitting
-    #textBlb = TextBlob(text)
-    #corrected_text = textBlb.correct()
+    # textBlb = TextBlob(text)
+    # corrected_text = textBlb.correct()
 
     split_text = text.split()
     new_text = []
@@ -77,42 +70,56 @@ def processing(text):
     return new_text
 
 
-def get_stop_words(tokens):
+def get_stop_words(all_tokens):
     '''
     Inputs:
-        - tokens (list of list of strings): output of tokenize(df)
+        - all_tokens (list of list of strings): a list of list of all tokens
 
-    Returns: 
+    Returns:
         - list of 10 most common words
     '''
-    all_words = list(itertools.chain.from_iterable(tokens))
+    all_words = list(itertools.chain.from_iterable(all_tokens))
     freq_dist = FreqDist(all_words)
     stop_words = freq_dist.most_common(10)
 
     return [word[0] for word in stop_words]
 
 
-def tokenize(df):
-    return [processing(text) for text in df.Text]
-
-
 def remove_stop(stop_words, tokens):
-    pass
-
-
-def make_ngrams(text, n):
     '''
-    Takes a tweet and create n-grams
+    Takes the list of words from a single review and removes
+    generated stop words
 
-    Input: 
-        tweet: a dictionary of a single tweet
-        n: the length of the tuple of strings
+    Input:
+        text (list of str): list of processed words in a review
 
-    Returns: a list of tuples of strings
+    Returns:
+        list of string representing words in a single review
     '''
 
-    text = processing(text)
-    ngrams = [tuple(text[i:i+n]) for i in range(len(text) - n + 1)]
+    for token in list(tokens):
+        if token in stop_words:
+            tokens.remove(token)
+
+    return tokens
+
+
+def make_ngrams(tokens, n):
+    '''
+    Takes the list of words from a single review and create n-grams
+
+    Input:
+        text (list of str): list of processed words in a review
+        n: the length of the words
+
+    Returns: list of 1 to n words strings for a single review
+    '''
+
+    ngrams = []
+
+    for i in range(1, n+1):
+        ngrams += [' '.join(tuple(tokens[j:j+i]))
+                   for j in range(len(tokens) - i + 1)]
 
     return ngrams
 
@@ -161,9 +168,32 @@ def compute_idf(docs):
     return idf_dict
 
 
+def augmented_freq(doc):
+    '''
+    Compute the augmented term frequency values of the tokens in a document
+
+    Inputs: 
+        doc: a list of tokens
+
+    Returns: dictionary that maps terms to their augmented frequency
+    '''
+
+    token_dict = count_tokens(doc)
+    tf_dict = {}
+    max_count = max(token_dict.values())
+
+    for token, count in token_dict.items():
+        tf_dict[token] = 0.5 + 0.5 * (count / max_count)
+
+    return tf_dict
+
+
 SAMPLE_DOCS = [['i', 'love', 'food', 'so', 'much'],
                ['good', 'service'],
                ['i', 'hate', 'this', 'restaurant']]
+
+sample = ['love food so much', 'good service', 'hate this restaurant']
+
 
 # SAMPLE_OUTPUT:
 #           i      love      food        so      much      good   service      hate      this  restaurant
@@ -191,17 +221,40 @@ def tfidf_vectorize(revs):
             tf = 0.5 + 0.5 * (rev[tok] / max_freq)
             rev[tok] = tf * idf[tok]
 
-    return pd.DataFrame(tok_to_freq_by_rev)
+    return pd.DataFrame(tok_to_freq_by_rev).fillna(0)
 
 
-def get_final_df(df):
-    tokens = tokenize(df)
-    stop_words = get_stop_words(tokens)
-    cleaned_tokens = remove_stop(stop_words, tokens)
+def get_final_df(csv_file, n, stop_removed):
+    '''
+    Given a dataframe with two columns, Rating and Text, 
+    returns a dataframe that vectorizes the text and joins it
+    back with the Rating column.
 
-    x_values = tfidf_vectorize(cleaned_tokens)
+    Inputs: 
+        df (pd df): the dataframe
+        n (int): range of  n-grams to use
+        stop_removed (boolean): True if stop words need to be removed
+
+    Returns: 
+        final dataframe for modelling
+    '''
+
+    df = pd.read_csv(csv_file)
+    all_tokens = [processing(text) for text in df.Text]
+
+    if stop_removed:
+        stop_words = get_stop_words(all_tokens)
+        all_tokens = [remove_stop(stop_words, tokens) for tokens in all_tokens]
+
+    ngrams = [make_ngrams(tokens, n) for tokens in all_tokens]
+
+    final_df = tfidf_vectorize(ngrams)
     y_values = df.Rating
 
-    final_df = x_values.insert(0, y_values)
+    final_df.insert(0, "Rating", y_values)
 
     return final_df
+
+
+# TESTING
+get_final_df("./test_data/babareba.csv", 2, False)

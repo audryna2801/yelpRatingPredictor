f8ac14652d1f7b37c50de083a1e5c68dcf186ee6
Zhang Qing Yang <zqy1998@Zhangs-MacBook-Pro.local>
2021-03-08 20:37:11 +0800

    bug fixes, function description

--- merge_data.py
+++ merge_data.py
@@ -13,10 +13,14 @@ def merge_data(scraped_data_dir="scraped_data/", num_samples=10000,
     '''
     First, combine all restaurant reviews into one dataframe.
     Then, group each review by rating and randomly sample 2,000
-    reviews from each group. Header argument is determined if
-    there is header column in each CSV file from scraped data.
+    reviews from each group. Merged dataset is then written into a csv.
 
-    Return: DataFrame with equal distribution of ratings
+    Input:
+        scraped_data_dir (dir): directory containing food reviews from 
+                                restaurants
+        num_samples (int): total number of reviews selected for merged dataset
+        random_state (int): seed for sampling reviews from each rating group 
+                            [1-5]
     '''
     # Get a list of all CSV filenames in scraped data directory
     all_rest_csv = [scraped_data_dir + file_name for file_name
@@ -24,18 +28,15 @@ def merge_data(scraped_data_dir="scraped_data/", num_samples=10000,
                     if file_name.endswith(".csv")]
 
     # Concatenate all DataFrames together
-    df_from_each_file = (pd.read_csv(f, usecols=[0, 1],
-                                     names=["Rating", "Text"],
-                                     header=None)
-                         for f in all_rest_csv)
+    df_from_each_file = (pd.read_csv(f) for f in all_rest_csv)
     concatenated_df = pd.concat(df_from_each_file, ignore_index=True)
+    print(concatenated_df)
 
     # Select (num_samples / 5) from each rating group
     num_samples_per_rating = round(num_samples / 5)
-    concat_data = (concatenated_df.groupby("Rating")
-                   .sample(n=num_samples_per_rating,
-                           random_state=random_state)
-                   .reset_index(drop=True))
+
+    concat_data = (concatenated_df.groupby("Rating").sample(
+        n=num_samples_per_rating, random_state=random_state).reset_index(drop=True))
 
     # Write concat_data to CSV
-    concat_data.to_csv("merged_data.csv", header=False, index=False)
+    concat_data.to_csv("merged_data_testing.csv", header=False, index=False)
